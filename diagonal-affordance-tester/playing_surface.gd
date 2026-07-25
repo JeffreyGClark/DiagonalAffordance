b59efcd4eaec93714c3e87d444613182346cb3fb
Jeff Clark <jeffreygclark@gmail.com>
2026-07-25 12:57:44 -0600

    Fixed window width override.

--- diagonal-affordance-tester/playing_surface.gd
+++ diagonal-affordance-tester/playing_surface.gd
@@ -1,2 +1,7 @@
 extends Node2D
 
+
+func _physics_process(_delta):
+	var raw_left_stick_vector: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down");
+	print(raw_left_stick_vector);
+	# if absf(raw_left_stick_vector[0]) > 0.383: # sin 22.5 degrees
